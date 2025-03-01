b5f8b3c46491c94f2c2a1f45079f9d0542e15bdf
lz6666-lz <74826074+lz6666-lz@users.noreply.github.com>
2025-03-01 12:06:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Plugin } = require("@saltcorn/data/models/plugin");
 
 module.exports = new Plugin({
+  sc_plugin_api_version: 1,
   name: "nextsalt",
   description: "Testowy plugin do Saltcorn",
   actions: {
